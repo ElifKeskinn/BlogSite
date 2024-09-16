9a7625a33319b20c981aebc576f1e1e0af32680b
Elif <elif.keskin233@gmail.com>
2024-09-16 14:03:50 +0300

    The general outline of the components is finished. Next is to create a home page and improve the design.

---
+++ src/components/CommentList.jsx
@@ -0,0 +1,23 @@
+export default function CommentList({ comments, setRefresh, refresh }) {
+    const handleLikeBtn = async (e) => {
+      e.preventDefault();
+      const commentId = e.target.value;
+      const response = await fetch(`http://localhost:3000/api/comments/${commentId}?like=true`);
+      if (!response.ok) {
+        return;
+      }
+      setRefresh(!refresh);
+    };
+  
+    return (
+      <div>
+        {comments?.reverse().map((comment, i) => (
+          <div key={i}>
+            <span>{comment.content}</span>
+            <button onClick={handleLikeBtn} value={comment.id}>Like {comment.likes}</button>
+            <button value={comment.id}>Dislike {comment.dislikes}</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
