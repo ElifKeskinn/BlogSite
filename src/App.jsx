d3c207a4e68092521229ccf8144a6a3fb16e43b6
Elif <elif.keskin233@gmail.com>
2024-09-18 19:27:00 +0300

    home component has written

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import './App.css';
 import { useState } from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import HomePage from "./components/HomePage";
 
 export default function App() {
-  const [page, setPage] = useState('home');
+  const [page, setPage] = useState(<HomePage />);
+
 
   const showPostList = () => setPage(<PostList setPage={setPage} />);
   const showPostForm = () => setPage(<PostForm setPage={setPage} />);
+  const showHomePage = () => setPage(<HomePage />);
 
   return (
     <div>
       <header>
+        <button onClick = {showHomePage}> 🏚️ </button>
         <button onClick={showPostList}>Post List</button>
         <button onClick={showPostForm}>New Post</button>
       </header>
       <main>
         {page}
-      </main>
+    </main>
     </div>
   );
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to Our Blog</h1>
+        <p>Explore fascinating posts and share your own ideas.</p>
+        <p>Stay tuned for daily updates on various topics from tech to lifestyle.</p>
+      </div>
+    );
+  }
+  
+ 
+  
